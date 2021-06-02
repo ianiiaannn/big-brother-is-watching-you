55ecb0b5a418b4d7cab3cf2eda56329d67533ebf
ianiiaannn <ianiiaannn@gmail.com>
2021-06-02 21:56:28 +0800

    make the script add some text to text

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-let pureHtml = document.documentElement.innerHTML;
-pureHtml.split('</head>').pop();
-console.log(pureHtml);
+let pureHtml = document.body.innerHTML;
+let count = 0;
+const treeWalker = document.createTreeWalker(document.body,NodeFilter.SHOW_TEXT);
+  while (treeWalker.nextNode()) {
+    const node = treeWalker.currentNode;
+    if (node.currentNode=="SCRIPT")console.log('hi')
+    console.log('loop')
+    node.textContent = node.textContent.replace(new RegExp('text', 'g'), '<span style="color:red !important">'+'text'+'</span>');
+}
