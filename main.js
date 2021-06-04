09db6091416dd4bfcfae121bf69c70dc601aa150
ianiiaannn <ianiiaannn@gmail.com>
2021-06-04 19:37:36 +0800

    update main

--- main.js
+++ main.js
@@ -35,9 +35,12 @@ txtFile.onreadystatechange=()=>{
         nodeCount++;
       }
     }
+    let alreadyWinnie = document.getElementsByClassName('winnie')
+    count+=alreadyWinnie.length;
     console.log(count);
     console.log(nodeCount)
     if(nodeCount/10<count){
+      console.log('winnie')
       let shepopo = [
         "https://image-resizer.cwg.tw/resize/uri/https%3A%2F%2Fcw1.tw%2FCW%2Fcrossing%2Fimages%2Farticle%2F201802%2Farticle-5a9368e289128.jpg/?w=828&format=webp",
         "https://imgcdn.cna.com.tw/www/webphotos/WebOg/600/20200128/598x314_858749950586.jpg",
