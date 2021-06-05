fe22b63b4c6b22df117e1914311494e426f72151
ianiiaannn <ianiiaannn@gmail.com>
2021-06-05 22:32:32 +0800

    update popup function

--- popup.js
+++ popup.js
@@ -8,9 +8,12 @@ let shepopo = [
   "https://img.komicolle.org/2020-04/15872145023946.jpg"
 ];
 let random_number = Math.floor(Math.random() * shepopo.length);
-document.getElementById('popuppic').src = shepopo[random_number];
+let pic = document.getElementById('popuppic');
+pic.src = shepopo[random_number];
 //document.getElementById('popuppic').style="width: 50%; height: 50%;zoom:50%";
-document.getElementById('popuppic').width='50%';
-document.getElementById('popuppic').height='50%';
-document.getElementById('popuppic').style='object-fit: cover;'
-document.body.style="width:auto; height: auto;";
+pic.onload=()=>{
+  console.log('hi')
+  pic.width=pic.naturalWidth*0.4;
+  pic.height=pic.naturalHeight*0.4;
+  document.body.style="width:auto; height: auto;";
+}
